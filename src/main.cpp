51febeea04a1e7ba79f0dc5e45b34180faf9d1f9
Issam Abkadri <iabkadri@student.1337.ma>
2024-11-05 14:34:22 +0100

    .

--- src/main.cpp
+++ src/main.cpp
@@ -7,17 +7,14 @@
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 2 || argc > 3) {
+    if (argc < 2 || argc > 3)
         usage_error(argv[0]);
-    }
+
     try {
         Match::match(argv[1], argv[2]);
-        //if (NFASim::match(argv[1], argv[2]))
-        //    std::cout << "accepted: " << argv[1] << '\n';
-        //else
-        //    std::cout << "not accepted: " << argv[1] << '\n';
     } catch (const std::exception& e) {
         std::cerr << e.what() << '\n';
+        std::exit(EXIT_FAILURE);
     }
-
+    std::exit(EXIT_SUCCESS);
 }
